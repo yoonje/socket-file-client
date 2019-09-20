4ba70e80e941ca2a1a68b39e5f2f9879e9403fe9
yoonje <elxm6123@gmail.com>
2019-09-20 16:34:49 +0900

    finish

--- main.c
+++ main.c
@@ -10,10 +10,10 @@
 
 int main(int argc, char *argv[]) {
     struct sockaddr_in serveraddr;
-    char *filename = basename(argv[1]); // filename만을 긁어 오는 함수
+    char *filename = basename(argv[1]); // get filename in path
     char buff[BUFFSIZE] = {0};
     strncpy(buff, filename, strlen(filename));
-    FILE *fp = fopen(argv[1], "rb"); // argv로 들어온 파일 path에 대하여 open
+    FILE *fp;
 
     int client_socket;
 
@@ -24,32 +24,32 @@ int main(int argc, char *argv[]) {
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     if (client_socket < 0) {
-        printf("Can't allocate client_socket");
+        printf("Can't allocate client_socket\n");
         exit(1);
     }
 
     if (inet_pton(AF_INET, "127.0.0.1", &serveraddr.sin_addr) < 0) { // 주소 정보 할당
-        printf("IPaddress Convert Error");
+        printf("IP address Convert Error\n");
         exit(1);
     }
 
     if (connect(client_socket, (const struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) {
-        printf("Connect Error");
+        printf("Connect Error\n");
         exit(1);
     }
 
     if (filename == NULL) {
-        printf("Can't get filename");
+        printf("Can't get filename\n");
+        exit(1);
+    }
+
+    if ((fp = fopen(argv[1], "rb")) == NULL) {
+        printf("Can't open file\n");
         exit(1);
     }
 
     if (send(client_socket, buff, BUFFSIZE, 0) == -1) {
-        printf("Can't send filename");
-        exit(1);
-    }
-
-    if (fp == NULL) {
-        printf("Can't open file");
+        printf("Can't send filename\n");
         exit(1);
     }
 
@@ -62,16 +62,15 @@ int main(int argc, char *argv[]) {
 
 void send_file(FILE *fp, int sockfd) {
     int n;
-    char line_buf[BUFFSIZE] = {0};
-    while ((n = fread(line_buf, sizeof(char), BUFFSIZE, fp)) > 0) {
+    char buff[BUFFSIZE] = {0};
+    while ((n = fread(buff, sizeof(char), BUFFSIZE, fp)) > 0) {
         if (n != BUFFSIZE && ferror(fp)) {
             printf("Read File Error");
             exit(1);
         }
-        if (send(sockfd, line_buf, n, 0) == -1) {
+        if (send(sockfd, buff, n, 0) == -1) {
             printf("Can't send file");
             exit(1);
         }
-        memset(line_buf, 0, BUFFSIZE);
     }
 }
