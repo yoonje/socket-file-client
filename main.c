3dfc6565086c7ec42f9592157b611eb39da997c1
yoonje <elxm6123@gmail.com>
2019-09-25 17:09:47 +0900

    add comment

--- main.c
+++ main.c
@@ -10,17 +10,19 @@
 
 int main(int argc, char *argv[]) {
     struct sockaddr_in serveraddr;
-    char *filename = basename(argv[1]);
+    char *filename = basename(argv[1]); // get filename
     char buff[BUFFSIZE] = {0};
     strncpy(buff, filename, strlen(filename));
     FILE *fp;
 
     int client_socket;
 
+    // initialize server info
     memset(&serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_port = htons(SERVERPORT);
 
+    // create client client_socket
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     if (client_socket < 0) {
@@ -28,11 +30,13 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    // initialize server info
     if (inet_pton(AF_INET, "127.0.0.1", &serveraddr.sin_addr) < 0) {
         printf("IP address Convert Error\n");
         exit(1);
     }
 
+    // connect server
     if (connect(client_socket, (const struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) {
         printf("Connect Error\n");
         exit(1);
@@ -43,16 +47,19 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    // file open
     if ((fp = fopen(argv[1], "rb")) == NULL) {
         printf("Can't open file\n");
         exit(1);
     }
 
+    // send filename
     if (send(client_socket, buff, BUFFSIZE, 0) == -1) {
         printf("Can't send filename\n");
         exit(1);
     }
 
+    // send file
     send_file(fp, client_socket);
 
     fclose(fp);
